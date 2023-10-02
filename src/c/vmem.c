65ebedd23500b60658e90b43b256a29d919ccd7e
Lowie Deferme <lowie.deferme@kuleuven.be>
2023-10-02 11:16:44 +0200

    feat: SLAT test verified on spike

--- src/c/vmem.c
+++ src/c/vmem.c
@@ -1,11 +1,11 @@
 #include "../encoding.h"
+#include "vmem_common.h"
 
 #ifndef __riscv_xlen
 #define RISCV_PGSHIFT 12
 #define RISCV_PGSIZE (0x1 << RISCV_PGSHIFT)
 #endif
 
-#define PAGECOUNT 4
 #define PTECOUNT RISCV_PGSIZE/8
 
 /**
@@ -14,20 +14,40 @@
 typedef unsigned long pte_t;
 
 /**
- * @brief Setup of (V)S-level page table
- * @details
- * Sets up a page table where:
- * - 0x000_0000_0000_0xxx is mapped to paddr_base as user page
- * - 0xfff_ffff_ffff_fxxx is mapped to paddr_base as supervisor page
- * @param pt_base Page table base address
+ * @brief Simplified setup of (V)S-level page table
+ *
+ * @details Sets up a page table where:
+ * - `0x000_0000_0000_0xxx` is mapped to paddr_base as user page
+ * - `0xfff_ffff_ffff_fxxx` is mapped to paddr_base as supervisor page
+ *
+ * @param pt Page table base address
+ * @param paddr_base Guest physical base address
  */
-void setup_spt(pte_t pt[PAGECOUNT][PTECOUNT], unsigned long paddr_base)
+void setup_spt(pte_t pt[5][PTECOUNT], unsigned long paddr_base)
+{
+  pt[0][0] = (SPA2GPA_C((pte_t)pt[1]) >> RISCV_PGSHIFT << PTE_PPN_SHIFT) | PTE_V;
+  pt[1][0] = (SPA2GPA_C((pte_t)pt[2]) >> RISCV_PGSHIFT << PTE_PPN_SHIFT) | PTE_V;
+  pt[2][0] = (SPA2GPA_C((pte_t)paddr_base) >> RISCV_PGSHIFT << PTE_PPN_SHIFT) | PTE_V | PTE_R | PTE_W | PTE_X | PTE_U | PTE_D | PTE_A;
+
+  pt[0][PTECOUNT-1] = (SPA2GPA_C((pte_t)pt[3]) >> RISCV_PGSHIFT << PTE_PPN_SHIFT) | PTE_V;
+  pt[3][PTECOUNT-1] = (SPA2GPA_C((pte_t)pt[4]) >> RISCV_PGSHIFT << PTE_PPN_SHIFT) | PTE_V;
+  pt[4][PTECOUNT-1] = (SPA2GPA_C((pte_t)paddr_base) >> RISCV_PGSHIFT << PTE_PPN_SHIFT) | PTE_V | PTE_R | PTE_W | PTE_X | PTE_D | PTE_A;
+}
+
+/**
+ * @brief Simplified setup of G-stage page table
+ *
+ * @details Sets up a page table where:
+ * - `0x000_0000_0000_0xxx` is mapped to paddr_base
+ * - `0x000_0000_002x_xxxx` is mapped to paddr_base superpage
+ *
+ * @param pt Page table base address
+ * @param paddr_base Supervisor physical base address
+ */
+void setup_gpt(pte_t pt[3][PTECOUNT], unsigned long paddr_base)
 {
   pt[0][0] = ((pte_t)pt[1] >> RISCV_PGSHIFT << PTE_PPN_SHIFT) | PTE_V;
   pt[1][0] = ((pte_t)pt[2] >> RISCV_PGSHIFT << PTE_PPN_SHIFT) | PTE_V;
+  pt[1][1] = ((pte_t)paddr_base >> RISCV_PGSHIFT << PTE_PPN_SHIFT) | PTE_V | PTE_R | PTE_W | PTE_X | PTE_U | PTE_D | PTE_A;
   pt[2][0] = ((pte_t)paddr_base >> RISCV_PGSHIFT << PTE_PPN_SHIFT) | PTE_V | PTE_R | PTE_W | PTE_X | PTE_U | PTE_D | PTE_A;
-
-  pt[0][PTECOUNT-1] = ((pte_t)pt[3] >> RISCV_PGSHIFT << PTE_PPN_SHIFT) | PTE_V;
-  pt[3][PTECOUNT-1] = ((pte_t)pt[4] >> RISCV_PGSHIFT << PTE_PPN_SHIFT) | PTE_V;
-  pt[4][PTECOUNT-1] = ((pte_t)paddr_base >> RISCV_PGSHIFT << PTE_PPN_SHIFT) | PTE_V | PTE_R | PTE_W | PTE_X | PTE_D | PTE_A;
 }
---
+++ src/macro/vmem_common.h
@@ -0,0 +1,27 @@
+/**
+ * @file vmem_common.h
+ * @brief Common vmem macros
+ * @version 0.1
+ * @date 2023-09-28
+ */
+
+/**
+ * @brief Convert a supervisor physical address to guest physical address
+ * @param spa_reg Register holding the supervisor physical address
+ * @par Clobbered registers
+ * - `t0`
+ * - `t1`
+ * @return The guest physical address inside `spa_reg`
+ */
+#define SPA2GPA_ASM(spa_reg) \
+  li t0, 0x00000000001fffff; \
+  li t1, 0x0000000000200000; \
+  and spa_reg, spa_reg, t0;  \
+  or spa_reg, spa_reg, t1
+
+/**
+ * @brief Convert a supervisor physical address to guest physical address
+ * @param spa Supervisor physical address to convert
+ * @return Guest physical address
+ */
+#define SPA2GPA_C(spa) ((spa & 0x00000000001fffff) | 0x0000000000200000)
