e60eae8e5ba70cb75fcb23aaea40e38a56f5f8b6
Lowie Deferme <lowie.deferme@kuleuven.be>
2023-10-04 14:45:12 +0200

    feat: check fetch, load, store in S-mode and U-mode

--- src/macro/vmem_common.h
+++ src/macro/vmem_common.h
@@ -5,6 +5,10 @@
  * @date 2023-09-28
  */
 
+#ifdef G_STAGE_AT
+
+#define GPA_BASE 0x0
+
 /**
  * @brief Convert a supervisor physical address to guest physical address
  * @param spa_reg Register holding the supervisor physical address
@@ -25,3 +29,13 @@
  * @return Guest physical address
  */
 #define SPA2GPA_C(spa) ((spa & 0x00000000001fffff) | 0x0000000000200000)
+
+#else
+
+#define GPA_BASE 0x80000000
+
+#define SPA2GPA_ASM(spa_reg) nop;
+
+#define SPA2GPA_C(spa) (spa)
+
+#endif
