0ef56a23fc7230cd11e2222a98778c1b82bcbc0a
Lowie Deferme <lowie.deferme@kuleuven.be>
2023-11-08 14:37:45 +0100

    feat: add other hypervisor instr

--- src/instructions.h
+++ src/instructions.h
@@ -7,12 +7,20 @@
 #define HFENCE_VVMA(asid, vaddr) .insn r 0x73, 0x0, 0x11, x0, vaddr, asid
 #define HFENCE_GVMA(vmid, gaddr) .insn r 0x73, 0x0, 0x31, x0, gaddr, vmid
 
-#define HLV_B(rd, rs1)  .insn r 0b1110011, 0b100, 0b0110000, rd, rs1, x0
-#define HLV_BU(rd, rs1) .insn r 0b1110011, 0b100, 0b0110000, rd, rs1, x1
-#define HLV_H(rd, rs1)  .insn r 0b1110011, 0b100, 0b0110010, rd, rs1, x0
-#define HLV_HU(rd, rs1) .insn r 0b1110011, 0b100, 0b0110010, rd, rs1, x1
-#define HLV_W(rd, rs1)  .insn r 0b1110011, 0b100, 0b0110100, rd, rs1, x0
-#define HLV_WU(rd, rs1) .insn r 0b1110011, 0b100, 0b0110100, rd, rs1, x1
-#define HLV_D(rd, rs1)  .insn r 0b1110011, 0b100, 0b0110110, rd, rs1, x0
+#define HLV_B(rd, vaddr)  .insn r 0b1110011, 0b100, 0b0110000, rd, vaddr, x0
+#define HLV_BU(rd, vaddr) .insn r 0b1110011, 0b100, 0b0110000, rd, vaddr, x1
+#define HLV_H(rd, vaddr)  .insn r 0b1110011, 0b100, 0b0110010, rd, vaddr, x0
+#define HLV_HU(rd, vaddr) .insn r 0b1110011, 0b100, 0b0110010, rd, vaddr, x1
+#define HLV_W(rd, vaddr)  .insn r 0b1110011, 0b100, 0b0110100, rd, vaddr, x0
+#define HLV_WU(rd, vaddr) .insn r 0b1110011, 0b100, 0b0110100, rd, vaddr, x1
+#define HLV_D(rd, vaddr)  .insn r 0b1110011, 0b100, 0b0110110, rd, vaddr, x0
+
+#define HSV_B(vaddr, data) .insn r 0b1110011, 0b100, 0b0110001, x0, vaddr, data
+#define HSV_H(vaddr, data) .insn r 0b1110011, 0b100, 0b0110011, x0, vaddr, data
+#define HSV_W(vaddr, data) .insn r 0b1110011, 0b100, 0b0110101, x0, vaddr, data
+#define HSV_D(vaddr, data) .insn r 0b1110011, 0b100, 0b0110111, x0, vaddr, data
+
+#define HLVX_HU(rd, vaddr) .insn r 0b1110011, 0b100, 0b0110010, rd, vaddr, x3
+#define HLVX_WU(rd, vaddr) .insn r 0b1110011, 0b100, 0b0110100, rd, vaddr, x3
 
 #endif
